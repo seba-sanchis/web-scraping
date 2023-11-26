64bb293f6fb03f59f8400169f26099ff0f111c76
seba-sanchis <seba.sanchis@gmail.com>
2023-11-26 15:15:00 -0300

    implement cron job

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -68,7 +68,7 @@ export async function scrapeProduct(url: string) {
       description,
       lowestPrice: Number(currentPrice) || Number(originalPrice),
       highestPrice: Number(originalPrice) || Number(currentPrice),
-      average: Number(currentPrice) || Number(originalPrice),
+      averagePrice: Number(currentPrice) || Number(originalPrice),
     };
 
     return data;
